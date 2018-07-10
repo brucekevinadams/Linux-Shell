cd4f8b5d41ff836c7e37e1aacd3afc42d1ee0127
Bruce Adams <22304833+brucekevinadams@users.noreply.github.com>
2018-07-10 13:34:03 -0500

    Update ReplaceFirstCapitalLetterWithPeriod.sh

--- ReplaceFirstCapitalLetterWithPeriod.sh
+++ ReplaceFirstCapitalLetterWithPeriod.sh
@@ -1,2 +1,9 @@
+# Author: Bruce Adams
+# austingamestudios.com
+# ezaroth@gmail.com
+# Linux Shell program to read input into an array
+# then replace the first letter with a period 
+# on each array word
+
 a=($(cat))
 echo ${a[@]/[A-Z]/.}
