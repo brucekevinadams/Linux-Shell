343e8c59e648df79872cc8fb827c8c5d5d26d1e7
Bruce Adams <22304833+brucekevinadams@users.noreply.github.com>
2018-07-10 13:56:54 -0500

    Create SelfConcatenateArray.sh

---
+++ SelfConcatenateArray.sh
@@ -0,0 +1,9 @@
+# Author: Bruce Adams
+# austingamestudios.com
+# ezaroth@gmail.com
+# Linux Shell program to read input into an array
+# then replace the first letter with a period 
+# on each array word
+
+a=$(paste -d ' ')
+echo $a $a $a
