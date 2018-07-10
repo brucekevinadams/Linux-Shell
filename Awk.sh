8028d1728e1194f7fe94003bfb598f31c8cc7262
Bruce Adams <22304833+brucekevinadams@users.noreply.github.com>
2018-07-10 15:20:45 -0500

    Update Awk.sh

--- Awk.sh
+++ Awk.sh
@@ -1,1 +1,19 @@
+
+# Author: Bruce Adams
+# austingamestudios.com
+# ezaroth@gmail.com
+# Linux Shell program using awk
+# Output Format
+#
+# For each row of data, append a space, a colon, followed by another space, and the grade. Observe the format showed 
+# in the sample output.
+#
+# Sample Input
+#
+# A 25 27 50
+# B 35 37 75
+# C 75 78 80
+# D 99 88 76
+
+
 awk '{avg=($2+$3+$4)/3; print $0, ":", (avg<50)?"FAIL":(avg<80)?"B":"A"}'
