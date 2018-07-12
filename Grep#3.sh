5e416832b23672856ea4205b1c9c8a6a62ab0283
Bruce Adams <22304833+brucekevinadams@users.noreply.github.com>
2018-07-12 07:24:25 -0500

    Update Grep#3.sh

--- Grep#3.sh
+++ Grep#3.sh
@@ -1,1 +1,9 @@
+# Author: Bruce Adams
+# austingamestudios.com
+# ezaroth@gmail.com
+# Linux Shell program using grep to remove a word
+# from the file. Note that the format should have
+# the name of the file after the word to be removed
+# Such that: grep -vi "that" Grep#3 would work as well
+
 grep -vi "that" 
