f0ed4d5f63974f2a5912d5994e67b6374e7826a9
Bruce Adams <22304833+brucekevinadams@users.noreply.github.com>
2018-07-12 07:49:11 -0500

    Update Grep_A.sh

--- Grep_A.sh
+++ Grep_A.sh
@@ -1,1 +1,7 @@
+# Author: Bruce Adams
+# austingamestudios.com
+# ezaroth@gmail.com
+# Linux Shell program using grep to retain only lines
+# from a file that contain the words the, that, then, those
+
 grep -Eiw 'the|that|then|those'
